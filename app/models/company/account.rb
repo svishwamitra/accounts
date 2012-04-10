e0c03df4fb15c22bde31db9018470c23656c0359
Srikanth <svishwamitra@gmail.com>
2012-04-10 17:58:51 +0530

    minor fixes

--- app/models/company/account.rb
+++ app/models/company/account.rb
@@ -4,7 +4,12 @@ module Company
     has_many :contacts, :through => :account_contacts, :class_name => "Crm::Contact"
     
     validates_presence_of :name
-    
+    validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i, :allow_blank => true, :allow_nil => true
+    #validates_format_of :phone, 
+    #                :with => /\A[0-9]{10}\Z/, 
+    #                :allow_blank => true, 
+    #                :allow_nil => true
+
     attr_accessible :contact_tokens,:name,:email,:phone
     attr_reader :contact_tokens
     acts_as_addressable
